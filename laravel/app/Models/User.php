aae7b1922585b73a2be30bf0a2f481d897d10fa2
ZakariaBouguera <zakaria.bouguera.sio@gmail.com>
2026-04-06 15:34:34 +0200

    GIT-09 update du fichier User.php

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -1,49 +1,49 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use PDO;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    private static $connexion = null;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    private function __construct()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        self::$connexion = new PDO('mysql:host=localhost;dbname=Safar', 'boss', 'azerty');
+    }
+
+    private static function getConnexion()
+    {
+        if (self::$connexion == null) {
+            new User();
+        }
+        return self::$connexion;
+    }
+
+    public static function getConnexionP($email, $mdp)
+    {
+        $bd = self::getConnexion();
+        $sql = "SELECT nom, prenom FROM client WHERE email = :email AND mdp = :mdp";
+        $st = $bd->prepare($sql);
+        $st->execute(array(':email' => $email, ':mdp' => $mdp));
+        $client = $st->fetch(PDO::FETCH_ASSOC);
+        $st->closeCursor();
+        return $client;
+    }
+
+    public static function programmerSejour($ville_dep, $ville_arr, $tarif, $numHotel, $numResp)
+    {
+        $bd = self::getConnexion();
+        $sql = 'insert into voyages values(:ville_depart, :ville_arriver, :tarif, :num_hotel, :num_responsable)';
+        $st = $bd->prepare($sql);
+        $st->execute(array(
+            ':ville_depart'      => $ville_dep,
+            ':ville_arriver'     => $ville_arr,
+            ':tarif'             => $tarif,
+            ':num_hotel'         => $numHotel,
+            ':num_responsable'   => $numResp
+        ));
+        $st->closeCursor();
     }
 }
